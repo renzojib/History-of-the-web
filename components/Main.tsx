a89a86e52f4e98bd9eef328a1cd503703764e994
pjancura <jancura.paul@gmail.com>
2024-10-02 20:06:37 -0400

    adds start of Main.tsx

---
+++ components/Main.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native-reanimated/lib/typescript/Animated";
+
+export default function Main() {
+    return (
+        <View>
+            <Text>
+                Hello from Main.tsx
+            </Text>
+        </View>
+    )
+}
